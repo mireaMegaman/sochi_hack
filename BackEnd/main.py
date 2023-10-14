0325e9c410d5935aa4b381c9bb390854bc822d5f
Kalinin Aleksandr <kalinin2003@list.ru>
2023-10-14 06:30:17 +0300

    Add work BackEnd

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -11,9 +11,16 @@ from pydantic import BaseModel
 from fastapi import BackgroundTasks, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
-from ml.classiffier import Classifier
+from ml.sochi_ml.main import generate_proba
 from ultralytics import YOLO
 from PIL import Image
+import torch
+import torch.nn.functional as F
+
+from transformers import (
+    TrOCRProcessor,
+    VisionEncoderDecoderModel
+)
 
 
 yolo = None
@@ -42,11 +49,10 @@ app.add_middleware(
 
 @app.on_event("startup")
 def startup_event():
-    global yolo, ocr
-    # yolo = YOLO(os.path.join('ml', 'yolo8nano_best_model.pt'))
-    yolo = YOLO(os.path.join('ml', 'pytorch_model.bin'))
-    # ocr = Classifier()
-    # ocr.get_model_from_file(os.path.join('ml', 'classifier_efficient_net_95-8acc.pt'))
+    global yolo, processor, ocr_model
+    yolo = YOLO(os.path.join('ml', 'best.pt'))
+    processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed')
+    ocr_model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-stage1')
 
 
 def to_zip(path: str):
@@ -68,22 +74,35 @@ def remove_file(path: str) -> None:
     rmtree(path)
 
 
+def predict(base_path: str):
+    image = Image.open(base_path).convert("RGB")
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    generated_ids = ocr_model.generate(
+        pixel_values,
+        output_scores=True,
+        return_dict_in_generate=True
+    )
+    ids, scores = generated_ids['sequences'], generated_ids['scores']
+    generated_text = processor.batch_decode(ids, skip_special_tokens=True)[0] # лейбл
+    probabilities = generate_proba(scores, ids, processor) # вероятности
+    return generated_text, probabilities
+
+
 @app.post('/get_result_64')
 def main_64(file: Image64, background: BackgroundTasks):
     session_id = uuid4()
-    path_crops = os.path.join('swans', str(session_id))
+    path_crops = os.path.join('sign', str(session_id))
     images = file.files
     json_ans = {"data": []}
     for i, file in enumerate(images):
         image_as_bytes = str.encode(file)  # convert string to bytes
         img_recovered = base64.b64decode(image_as_bytes)  # decode base64string
         image = Image.open(io.BytesIO(img_recovered))
-        # results = yolo.predict(image)
-        # for el in results:
-        #     print(el.boxes)
-        #     el.save_crop(path_crops)
-        predict = yolo.predict(image)
-        json_ans['data'].append({"file_name" : f"file_{i+1}", "pred_class" : predict})
+        crops = yolo.predict(image)
+        for el in crops:
+            el.save_crop(path_crops)
+        text, probabilities = predict(os.path.join(path_crops, 'sign', 'im.jpg'))
+        json_ans['data'].append({'text': text, 'probabilities': probabilities})
     with open(os.path.join(path_crops, 'data.txt'), 'w') as outfile:
         json.dump(json_ans, outfile)
     background.add_task(remove_file, path_crops)
